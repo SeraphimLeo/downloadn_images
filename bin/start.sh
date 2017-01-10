6e4c9e4718626857e67b45b53feacaa03caf1d0e
CrazyCode <seraphim.leo@gmail.com>
2017-01-10 12:13:02 +0800

    修改 macos 下的启动脚本

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+
+BASEDIR=$(pwd)/../
+VENVDIR=$BASEDIR/venv
+
+export PATH=$PATH:$BASEDIR/ext/phantomjs-2.1.1-macosx/bin
+source $VENVDIR/bin/activate
+
 python ../main/tmall.py
